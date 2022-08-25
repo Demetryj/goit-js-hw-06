ebef6d213de6462aab2b13c7119c96c8bff5178a
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-08-25 21:35:44 +0300

    Update task-02.js
    add task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const listIngredients = document.querySelector('#ingredients');
+// console.log(listIngredients);
+
+const itemsOfListIngredients = ingredients.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = ingredient;
+  itemEl.classList.add('item');
+  return itemEl;
+});
+// console.log(itemsOfListIngredients);
+listIngredients.prepend(...itemsOfListIngredients);
