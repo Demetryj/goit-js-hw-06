ce472828f8b2c055c1372f1cdb91a9f196426f92
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-08-25 20:28:38 +0300

    create rep and copy files

---
+++ js/task-01.js
@@ -0,0 +1,18 @@
+const categories = document.querySelectorAll('#categories li.item');
+const quantityCategories = categories.length;
+console.log(`Number of categories: ${quantityCategories}`);
+console.log('');
+
+const getResult = function (categories) {
+  categories.forEach(category => {
+    const titleOfCategoty = category.firstElementChild.textContent;
+    const numbersOfElements = category.lastElementChild.children.length;
+    console.log(`Category: ${titleOfCategoty}`);
+    console.log(`Elements: ${numbersOfElements}`);
+    console.log('');
+    return;
+  });
+  return;
+};
+
+getResult(categories);
