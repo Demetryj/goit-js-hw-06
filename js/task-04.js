ebb91c3d8c3fcc4184451642738a4ee090bfe7e2
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-08-26 19:19:59 +0300

    add task-4

---
+++ js/task-04.js
@@ -0,0 +1,26 @@
+/* Створи змінну counterValue, в якій буде зберігатися поточне значення 
+     лічильника та ініціалізуй її значенням 0.
+     Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй 
+     значення лічильника.
+     Оновлюй інтерфейс новим значенням змінної counterValue. */
+
+let counterValue = 0;
+
+const buttonDecrEl = document.querySelector('[data-action="decrement"]');
+console.log(buttonDecrEl);
+const buttonIncrEl = document.querySelector('[data-action="increment"]');
+console.log(buttonIncrEl);
+const result = document.querySelector('#value');
+
+buttonDecrEl.addEventListener('click', event => {
+  counterValue -= 1;
+  result.textContent = counterValue;
+  return;
+});
+
+buttonIncrEl.addEventListener('click', event => {
+  counterValue += 1;
+
+  result.textContent = counterValue;
+  return;
+});
